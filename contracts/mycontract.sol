1d35fd13b111a51b532a9212913cc1b547a111e9
Gavinlai01 <laichikwan01@gmail.com>
2022-11-25 16:45:35 +0800

    commit

---
+++ contracts/mycontract.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: Gavin
+pragma solidity ^0.8.0;
+
+contract Property{
+    uint private price;
+    address public owner;
+
+    constructor(){
+        price = 0;
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+
+    function changeOwner(address _owner) public onlyOwner {
+        owner = _owner;
+    }
+
+    function setPrice(uint _price) public{
+        price = _price;
+    }
+
+    function getPrice() view public returns (uint) {
+        return price;
+    }
+    event OwnerChanged(address owner);
+}
+
+// 0xa131AD247055FD2e2aA8b156A11bdEc81b9eAD95
